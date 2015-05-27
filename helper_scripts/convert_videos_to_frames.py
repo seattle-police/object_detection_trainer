2f40737908b94b0aadb9466020244434f54e38b5
timothyclemansspd <timothy.clemans@seattle.gov>
2015-05-27 10:44:25 -0700

    Make to_folder use FRAMES_DIR

--- helper_scripts/convert_videos_to_frames.py
+++ helper_scripts/convert_videos_to_frames.py
@@ -1,9 +1,11 @@
 import os
 import sys
-
+from object_detection_trainer import settings
 from_folder = sys.argv[1]
-to_folder = sys.argv[2]
+to_folder = settings.FRAMES_DIR
+if not (to_folder[-1] in ['/', '\\']):
+    to_folder = to_folder + '/'
 
 videos = sorted(os.listdir(from_folder))
 for i, video in enumerate(videos):
-    os.system('ffmpeg -threads 0 -i "%s%s" "/datadrive/%s_%%6d.png"' % (from_folder, video, to_folder, i))
+    os.system('ffmpeg -threads 0 -i "%s%s" "%s%s_%%6d.png"' % (from_folder, video, to_folder, i))
